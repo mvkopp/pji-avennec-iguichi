7658651091d79f35f35b9c319b241ca8202ffb06
Avennec Mael <mael.avennec@hotmail.fr>
2020-05-16 14:51:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,17 +9,20 @@ def main_by_author(author_name):
     :params: - author_name (str) the author name
     :returns: /
     """
+
+    CRISTAL_members = scrape_scientist.recover_all_cristal_members_with_infos()
+
     print('\n----'+len(author_name)*'-'+'----\n'+'*** '+author_name+' ***\n'+'----'+len(author_name)*'-'+'----')
     
     # DBLP
     print("\n--------\n| DBLP |\n--------\n")
-    publications_from_dblp = dblp_api.get_articles_by_author(author_name)
+    publications_from_dblp = dblp_api.get_articles_by_author(author_name,CRISTAL_members)
     dblp_api.save_articles_into_database(publications_from_dblp,author_name,True)
     dblp_api.test_accuracy_author_name(author_name,publications_from_dblp,True)
 
     # Crossref
     print("\n------------\n| Crossref |\n------------\n")
-    publications_from_crossref = crossref_api.get_articles_by_author(author_name)
+    publications_from_crossref = crossref_api.get_articles_by_author(author_name,CRISTAL_members)
     crossref_api.save_articles_into_database(publications_from_crossref,author_name,True)
     crossref_api.test_accuracy_author_name(author_name,publications_from_crossref,True)
 
@@ -33,20 +36,24 @@ def main():
     :returns: /
     """
     CRISTAL_permanent_members = scrape_scientist.recover_all_cristal_members()['permanent']
+    CRISTAL_non_permanent_members = scrape_scientist.recover_all_cristal_members()['permanent']
 
-    for member_name in CRISTAL_permanent_members :
+    CRISTAL_members = scrape_scientist.recover_all_cristal_members_with_infos()
+
+    for member_name in CRISTAL_members :
+
         print('________________________________________________________________')
         print('\n----'+len(member_name)*'-'+'----\n'+'*** '+member_name+' ***\n'+'----'+len(member_name)*'-'+'----')
-    
+            
         # DBLP
         print("\n--------\n| DBLP |\n--------\n")
-        publications_from_dblp = dblp_api.get_articles_by_author(member_name)
+        publications_from_dblp = dblp_api.get_articles_by_author(member_name,CRISTAL_members)
         dblp_api.save_articles_into_database(publications_from_dblp,member_name,True)
         dblp_api.test_accuracy_author_name(member_name,publications_from_dblp,True)
 
         # Crossref
         print("\n------------\n| Crossref |\n------------\n")
-        publications_from_crossref = crossref_api.get_articles_by_author(member_name)
+        publications_from_crossref = crossref_api.get_articles_by_author(member_name,CRISTAL_members)
         crossref_api.save_articles_into_database(publications_from_crossref,member_name,True)
         crossref_api.test_accuracy_author_name(member_name,publications_from_crossref,True)
     
@@ -54,6 +61,5 @@ def main():
 if __name__ == "__main__": # execute only if run as a script
     
     #author_name=input("Write an author name : ")
-    #main_by_author(author_name)
-
+    #main_by_author('Laetitia Jourdan')
     main()
